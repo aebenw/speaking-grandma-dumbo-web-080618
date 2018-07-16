5716b71421f69b857f1771e398011879abb99b01
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-16 20:43:35 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(str)
-  if str == str.upcase
-    p "NO, NOT SINCE 1938!"
-  elsif str == "I LOVE YOU GRANDMA!"
+  if str == "I LOVE YOU GRANDMA!"
     p "I LOVE YOU TOO PUMPKIN!"
+  elsif str == str.upcase
+    p "NO, NOT SINCE 1938!"
   else 
     p "HUH?! SPEAK UP, SONNY!"
   end
